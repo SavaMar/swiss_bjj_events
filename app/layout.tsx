6ea72b09f186ff8267c5f046c580f95467efe038
MariSava <savamar33@gmail.com>
2025-04-22 17:11:18 +0200

    Fix picture wabsite preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Swiss BJJ",
   description: "Swiss Brazilian Jiu-Jitsu Community Hub",
+  openGraph: {
+    title: "Swiss BJJ Events & Gyms",
+    description: "Find the best BJJ events and gyms in Switzerland.",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Swiss BJJ Preview",
+      },
+    ],
+    type: "website",
+    siteName: "Swiss BJJ",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Swiss BJJ Events & Gyms",
+    description: "Find the best BJJ events and gyms in Switzerland.",
+    images: ["/preview.png"],
+  },
 };
 
 export default function RootLayout({
