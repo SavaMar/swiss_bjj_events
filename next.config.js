dce4ef72af798a08dcfbf80f9b38cc7e78b92e16
MariSava <savamar33@gmail.com>
2025-05-01 18:05:23 +0200

    Still fixing img showing up for .md

--- next.config.js
+++ next.config.js
@@ -6,11 +6,12 @@ const nextConfig = {
   },
   // Configure allowed image domains with remotePatterns (more secure approach)
   images: {
+    domains: ["filedn.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "filedn.com",
-        pathname: "**",
+        pathname: "/lPmOLyYLDG0bQGSveFAL3WB/bjj%20logos/**",
       },
       {
         protocol: "https",
